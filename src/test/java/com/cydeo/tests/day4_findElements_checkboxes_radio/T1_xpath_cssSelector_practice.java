3fdd36f75da9b14a490de12fbaa1a99bfab001a2
Waleed <waleedffx@gmail.com>
2022-02-11 11:54:02 -0600

    Day4 find elements and check boxes

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -1,7 +1,57 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class T1_xpath_cssSelector_practice {
     public static void main(String[] args) {
+
+        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: XPATH and cssSelector Practices
+        //1. Open Chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2. Go to https://practice.cydeo.com/forgot_password
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        //3. Locate all the WebElements on the page using XPATH and/or CSS
+        //locator only (total of 6)
+
+        //a. “Home” link
+        //we will use several options below
+        WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class='nav-link']"));
+        WebElement homeLink_ex2 = driver.findElement(By.cssSelector("a.nav-link"));
+        WebElement homeLink_ex3 = driver.findElement(By.cssSelector("a[href='/']"));
+
+
+        //b. “Forgot password” header
+        //Locate header using cssSelector: locate parent element and move down to h2
+        WebElement header_ex1 = driver.findElement(By.cssSelector("div.example > h2"));
+
+        //Locate header using xpath, and using web elements text "Forgot Password"
+        //WebElement header_ex2 = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+        WebElement header_ex2 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
+//        String str = header_ex2.getText();
+//        System.out.println("str="+str);
+
+        //c. “E-mail” text
+        WebElement emailLabel = driver.findElement(By.xpath("//label[@for='email']"));
+
+        //d. E-mail input box
+        WebElement inputBox_ex1 = driver.findElement(By.xpath("//input[@name='email']"));
+
+        //locate using xpath contains method
+        WebElement inputBox_ex2 = driver.findElement(By.xpath("//input[contains(@pattern, 'a-z')]"));
+
+
+
+        //e. “Retrieve password” button
+        //f. “Powered by Cydeo text
+        //4. Verify all web elements are displayed.
+
 
     }
 }
