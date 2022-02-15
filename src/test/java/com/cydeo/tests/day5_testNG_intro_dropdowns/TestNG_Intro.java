919bc088fbc54f786af10dd8d798bde3385c7be7
Waleed <waleedffx@gmail.com>
2022-02-15 14:16:33 -0600

    Day5 testNG intro

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.*;
+
+public class TestNG_Intro {
+
+    @BeforeClass
+    public void setupClass(){
+        System.out.println("----> BeforeClass is running");
+    }
+
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("----> AfterClass is running");
+    }
+
+    @BeforeMethod
+    public void setUpMethod(){
+        System.out.println("Before method is running");
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        System.out.println("AfterMethod is running");
+    }
+
+    @Test //(priority = 1)
+    public void test1(){
+        System.out.println("Test 1 is running");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running");
+    }
+
+
+}
